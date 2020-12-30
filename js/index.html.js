b81750be9e63dc3001a354cc7d0996dc1c791225
tryhwee <76719556+tryhwee@users.noreply.github.com>
2020-12-29 20:04:18 -0800

    Update index.html.js

--- js/index.html.js
+++ js/index.html.js
@@ -122,11 +122,11 @@ document.documentElement.innerHTML='\
 			padding-top: 0.5em;\
 		}\
 	</style>\
-	<script type="text/javascript" src="https://seedmanc.github.io/Booru-mass-uploader/js/common-min.js?v=1.4.4"></script>\
+	<script type="text/javascript" src="https://raw.githubusercontent.com/tryhwee/Booru-mass-uploader/gh-pages/js/common-min.js?v=1.4.4"></script>\
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>\
-	<script type="text/javascript" src="https://seedmanc.github.io/Booru-mass-uploader/js/booru-params.js?v=1.4.4"></script>\
-	<script type="text/javascript" src="https://seedmanc.github.io/Booru-mass-uploader/js/helpers.min.js?v=1.4.5"></script>\
-	<script type="text/javascript" src="https://seedmanc.github.io/Booru-mass-uploader/js/uploader.js?v=1.4.4"></script>\
+	<script type="text/javascript" src="https://raw.githubusercontent.com/tryhwee/Booru-mass-uploader/gh-pages/js/booru-params.js?v=1.4.4"></script>\
+	<script type="text/javascript" src="https://raw.githubusercontent.com/tryhwee/Booru-mass-uploader/gh-pages/js/helpers.min.js?v=1.4.5"></script>\
+	<script type="text/javascript" src="https://raw.githubusercontent.com/tryhwee/Booru-mass-uploader/gh-pages/js/uploader.js?v=1.4.4"></script>\
 </head>\
 <body>\
 <h1>Booru Mass-Uploader<span style="font-size:14px;"> v1.4.5</span></h1>\
